d7051d4c1da45a143223b7a999fe63a07ae168c2
Chase <chaseaohill@gmail.com>
2023-02-20 16:32:30 -0500

    Created model and updated storyboarrd

---
+++ CoinBrowser/CoinBrowser/Controllers/CoinController.swift
@@ -0,0 +1,12 @@
+//
+//  CoinController.swift
+//  CoinBrowser
+//
+//  Created by Chase on 2/20/23.
+//
+
+import Foundation
+
+class CoinController {
+    
+}
---
+++ CoinBrowser/CoinBrowser/Supporting Files/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CoinBrowser
+//
+//  Created by Chase on 2/20/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct CoinGecko {
+        static let baseURL =
+        
+    }
+}
