4093a619481f716785742bb043d5f30d007a2c52
Chase <chaseaohill@gmail.com>
2023-02-20 18:02:14 -0500

    Need to finish final touches

--- CoinBrowser/CoinBrowser/Controllers/CoinController.swift
+++ CoinBrowser/CoinBrowser/Controllers/CoinController.swift
@@ -9,4 +9,43 @@ import Foundation
 
 class CoinController {
     
+    // MARK: - Properties
+    
+    static var coins: [Coin] = []
+    
+    // MARK: - Functions
+    
+    static func fetchCoins(completion: @escaping (Bool) -> Void) {
+        guard let baseURL = URL(string: Constants.CoinGecko.baseURL) else { return completion(false) }
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.path.append(Constants.CoinGecko.coinPath)
+        
+        guard let finalURl = urlComponents?.url else { return }
+        print(finalURl)
+        URLSession.shared.dataTask(with: finalURl) { coinData, response, error in
+            
+            if let error = error {
+                print("There was an error: \(error.localizedDescription)")
+                completion(false)
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Response Status Code: \(response.statusCode)")
+            }
+            
+            guard let data = coinData else { return completion(false) }
+            do {
+                
+                if let topLevel = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [[String : String]] {
+                    for coinDictionary in topLevel {
+                            guard let parsedCoin = Coin(dictionary: coinDictionary) else { return }
+                            coins.append(parsedCoin)
+                    }
+                    completion(true)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        } .resume()
+    }
 }
--- CoinBrowser/CoinBrowser/Controllers/CoinTableViewController.swift
+++ CoinBrowser/CoinBrowser/Controllers/CoinTableViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class CoinTableViewController: UITableViewController {
 
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        CoinController.fetchCoins { success in
+            if success {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return CoinController.coins.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "coinCell", for: indexPath)
+        
+        let coin = CoinController.coins[indexPath.row]
 
-        // Configure the cell...
+        var config = cell.defaultContentConfiguration()
+        config.text = coin.name
+        config.secondaryText = "Symbol \(coin.symbol), id: \(coin.id)"
+        cell.contentConfiguration = config
 
         return cell
     }
-
 }
